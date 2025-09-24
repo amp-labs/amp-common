98783f763c96ba43c5ba4c2841b6db30a107acb3
Chris Eberle <chris@withampersand.com>
2025-09-23 19:28:07 -0700

    add context

--- utils/context.go
+++ utils/context.go
@@ -16,3 +16,31 @@ func IsContextAlive(ctx context.Context) bool {
 		return true
 	}
 }
+
+func AddContextValue[K any, V any](ctx context.Context, key K, value V) context.Context {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	return context.WithValue(ctx, key, value)
+}
+
+func ExtractContextValue[K any, V any](ctx context.Context, key K) (V, bool) {
+	var zero V
+
+	if ctx == nil {
+		return zero, false
+	}
+
+	val := ctx.Value(key)
+	if val == nil {
+		return zero, false
+	}
+
+	v, ok := val.(V)
+	if !ok {
+		return zero, false
+	}
+
+	return v, true
+}

a40f9bd4123eecbadeab5d5e00fff1f640cf9985
Chris Eberle <chris@withampersand.com>
2025-11-25 18:33:31 -0800

    add using (#51)
    * add using
    
    * fix unit tests

--- using/using.go
+++ using/using.go
@@ -26,8 +26,10 @@ var (
 
 // NewResource creates a Resource from a function that returns a value, closer, and error.
 // The closer will be automatically invoked when the resource is used via Use().
-func NewResource[V any](f func() (V, Closer, error)) Resource[V] {
-	return f
+func NewResource[V any](f func() (V, Closer, error)) *Resource[V] {
+	return &Resource[V]{
+		create: f,
+	}
 }
 
 // Closer is a function that cleans up a resource. It follows the same signature as io.Closer.Close().
@@ -35,16 +37,33 @@ type Closer func() error
 
 // Resource represents a managed resource that can be used safely with automatic cleanup.
 // It is a function that produces a value, a closer for that value, and potentially an error.
-type Resource[V any] func() (V, Closer, error)
+type Resource[V any] struct {
+	create   func() (V, Closer, error)
+	released bool
+}
 
 // Use executes the provided function with the resource value, ensuring the resource
 // is properly closed afterward. If both the function and closer return errors, both
 // are collected and returned as a combined error.
-func (p Resource[V]) Use(userFunc func(value V) error) (errOut error) {
+func (p *Resource[V]) Use(userFunc func(value V) error) (errOut error) {
+	if p == nil {
+		return ErrResourceNil
+	}
+
+	p.released = false
+
 	return use(p, userFunc)
 }
 
-func use[V any](resource Resource[V], userFunc func(value V) error) (errOut error) {
+// Release marks the resource as released, preventing automatic cleanup.
+// When called, the resource's closer will not be invoked when Use() completes.
+// This is useful when you need to transfer ownership of the resource outside
+// the Use() block and manage its lifecycle manually.
+func (p *Resource[V]) Release() {
+	p.released = true
+}
+
+func use[V any](resource *Resource[V], userFunc func(value V) error) (errOut error) {
 	if resource == nil {
 		return ErrResourceNil
 	}
@@ -53,14 +72,14 @@ func use[V any](resource Resource[V], userFunc func(value V) error) (errOut erro
 		return ErrFuncNil
 	}
 
-	val, closer, err := resource()
+	val, closer, err := resource.create()
 	if err != nil {
 		return err
 	}
 
 	errs := errors2.Collection{}
 	defer func() {
-		if closer != nil {
+		if !resource.released && closer != nil {
 			if e := closer(); e != nil {
 				errs.Add(e)
 			}

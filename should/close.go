7d52e0f1bf44a22a405cb4a948ae7feb0b1499d7
Chris Eberle <chris@withampersand.com>
2025-12-06 14:37:40 -0800

    Make the inputs to should.Close more flexible (#65)

--- should/close.go
+++ should/close.go
@@ -4,22 +4,54 @@
 package should
 
 import (
+	"fmt"
 	"io"
 	"log/slog"
 	"os"
+
+	"github.com/amp-labs/amp-common/logger"
 )
 
 // Close attempts to close the given io.Closer and logs an error if it fails.
 // This is useful for cleanup in defer statements where you want to ensure
 // resources are closed but don't want to complicate error handling.
 //
-// Example:
+// The args parameter is optional and can be used in three ways:
+//   - No args: uses a default error message
+//   - One arg: treated as the error message
+//   - Multiple args: first arg is a format string, remaining args are formatting values
 //
-//	defer should.Close(file, "failed to close file")
-func Close(closer io.Closer, msg string) {
+// Examples:
+//
+//	defer should.Close(file)                                    // uses default message
+//	defer should.Close(file, "failed to close file")            // simple message
+//	defer should.Close(file, "failed to close %s", filename)    // formatted message
+func Close(closer io.Closer, args ...any) {
 	if err := closer.Close(); err != nil {
-		slog.Error(msg, "error", err)
+		msg := argsToMessage(args)
+		if msg == "" {
+			logger.Get().Error("error closing io.Closer", "error", err)
+		} else {
+			logger.Get().Error(msg, "error", err)
+		}
 	}
+}
+
+// argsToMessage converts variadic args into a formatted message string.
+// Returns empty string if no args, Sprint if one arg, or Sprintf if multiple args.
+func argsToMessage(args []any) string {
+	if len(args) == 0 {
+		return ""
+	}
+
+	if len(args) == 1 {
+		return fmt.Sprint(args[0])
+	}
+
+	fmtStr := fmt.Sprint(args[0])
+	remaining := args[1:]
+
+	return fmt.Sprintf(fmtStr, remaining...)
 }
 
 // Remove attempts to remove the named file or directory and logs an error if it fails.
